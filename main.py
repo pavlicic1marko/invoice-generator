bffb8e8525c68afda78bd1bc0d3b063dde261334
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-01-10 11:48:25 +0100

    adding labels, buttons

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
-# This is a sample Python script.
+from tkinter import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+window = Tk()
+
+window.title("Invoice Generator")
+
+medicine_lable = Label(window, text="Medicine: ")
+medicine_lable.pack()
+
+medicine_listbox = Listbox(window, selectmode=SINGLE)
+medicine_listbox.pack()
+
+quantity_label = Label(window, text="Quantity")
+quantity_label.pack()
+
+quantity_entry = Entry(window)
+quantity_entry.pack()
+
+add_button = Button(text="add medicine")
+add_button.pack()
+
+total_amount_label = Label(window, text="Total Amount")
+total_amount_label.pack()
+
+total_amount_entry = Entry(window)
+total_amount_entry.pack()
+
+customer_label = Label(window, text="Customer Name:")
+customer_label.pack()
+
+customer_entry = Entry(window)
+customer_entry.pack()
+
+generate_button = Button(window, text="Generate Invoice")
+generate_button.pack()
+
+invoice_text = Text(window, height=10, width=50)
+invoice_text.pack()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+window.mainloop()
 
+#if __name__ == '__main__':
+    #print_hi('PyCharm')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
